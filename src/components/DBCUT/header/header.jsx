b31c7f164fd422d8c482507f263e972985f12136
biganea <dhk21512@gmail.com>
2021-08-27 12:20:10 +0900

    header before css

--- src/components/DBCUT/header/header.jsx
+++ src/components/DBCUT/header/header.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import Navbar from "../navbar/navbar";
 import styles from "./header.module.css";
 
 const Header = (props) => {
+  const history = useHistory();
+
   return (
     <div className={styles.header}>
-      <h1>Header</h1>
+      <Link to="/" className={styles.logo}>
+        <img src="https://www.dbcut.com/bbs/template/header/h1000_250/image/top_logo.gif" alt="logo" />
+      </Link>
+      <ul className={styles.topmenu}>
+        <li className={styles.item}>현재접속회원:0명</li>
+        <li className={styles.item}>매니아랭킹</li>
+        <li className={styles.item}>최근코멘트</li>
+        <li className={styles.item}>회원가입</li>
+      </ul>
+      <Navbar />
     </div>
   );
 };
