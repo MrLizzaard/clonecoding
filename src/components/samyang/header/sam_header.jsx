69ae472b467ac2fb05c654be8ad22bd3e93cb768
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-31 18:11:31 +0900

    header init

---
+++ src/components/samyang/header/sam_header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./sam_header.module.css";
+
+const SamHeader = (props) => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.sec1}></div>
+      <div className={styles.sec2}></div>
+    </div>
+  );
+};
+
+export default SamHeader;
--- src/components/samyang/sam_main/sam_main.jsx
+++ src/components/samyang/sam_main/sam_main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SamHeader from "../header/sam_header";
 import styles from "./sam_main.module.css";
 
 const SamMain = (props) => {
@@ -10,6 +11,7 @@ const SamMain = (props) => {
           리뉴얼 런칭 기념 <span>30% 할인 쿠폰 증정</span>
         </Link>
       </div>
+      <SamHeader />
     </div>
   );
 };
