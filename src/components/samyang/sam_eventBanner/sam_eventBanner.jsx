0effc463fe55803d47a2c82524f6176dcde9665e
MrLizzaard <dhk21512@gmail.com>
2021-09-07 18:59:06 +0900

    sam newarrival & sam eventbanner

---
+++ src/components/samyang/sam_eventBanner/sam_eventBanner.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SamEventBanner = (props) => <h1>SamEventBanner</h1>;
+
+export default SamEventBanner;
--- src/components/samyang/sam_mainContents/sam_mainContents.jsx
+++ src/components/samyang/sam_mainContents/sam_mainContents.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./sam_mainContents.module.css";
 import SamNewArrival from "../sam-new-arrival/sam_newArrival";
+import SamEventBanner from "../sam_eventBanner/sam_eventBanner";
 
 const SamMainContents = (props) => (
   <div className={styles.mainContents}>
     <SamNewArrival />
+    <SamEventBanner />
   </div>
 );
 
