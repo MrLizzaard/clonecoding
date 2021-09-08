82aa1c9bddf79730cc65d9cf6e74c9d90824cbdc
MrLizzaard <dhk21512@gmail.com>
2021-09-08 11:24:08 +0900

    display1

---
+++ src/components/samyang/sam_display1/sam_display1.jsx
@@ -0,0 +1,262 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./sam_display1.module.css";
+
+const SamDisplay1 = (props) => (
+  <div className={styles.display1}>
+    <div className={styles.listContainer}>
+      <ul className={styles.prdList}>
+        <li style={{ background: "rgb(206, 218, 255)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/208_thum_5.png" alt="불닭" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(228, 244, 252)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/202108/6e441b07382d1c3be947ace8e6e57894.png" alt="전병" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(255, 231, 207)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/511_thum_4.png" alt="맛있는라면" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(255, 242, 207)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/62_thum.png" alt="사또밥" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className={styles.mainTitle}>
+            <div className={styles.title1}>Best Item</div>
+            <div className={styles.title2}>
+              가장 많이 팔리는<span>베스트상품</span>
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(228, 212, 225)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/602_thum_2.png" alt="불닭만두" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(255, 231, 200)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/202108/f344c334ae2d48bf2da53cc8ed8e174c.png" alt="밀크전병" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(255, 209, 198)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="	https://www.sydeliciousshop.com/web/product/big/416_thum_4.png" alt="불닭까르보" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+        <li style={{ background: "rgb(255, 242, 207)" }}>
+          <div className={styles.box}>
+            <div className={styles.thumbnail}>
+              <div className={styles.prdImg}>
+                <Link>
+                  <img src="https://www.sydeliciousshop.com/web/product/big/543_thum.png" alt="치즈붉닭" />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.description}>
+              <div className={styles.name}>
+                <Link>
+                  <span>[삼양맛샵] 불닭볶음면 140g x 5입</span>
+                </Link>
+              </div>
+              <div className={styles.saleBox}>
+                <div className={styles.sale_bg}>
+                  <span className={styles.saleText}>20%</span>
+                </div>
+              </div>
+              <ul className={styles.spec}>
+                <li className={styles.salesPrice}>4600원</li>
+                <li className={styles.customerPrice}>5750원</li>
+              </ul>
+            </div>
+            <div className={styles.icon}>
+              <img src="https://www.sydeliciousshop.com/web/upload/icon_202108241601308600.png" alt="장바구니" />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default SamDisplay1;
--- src/components/samyang/sam_mainContents/sam_mainContents.jsx
+++ src/components/samyang/sam_mainContents/sam_mainContents.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./sam_mainContents.module.css";
 import SamNewArrival from "../sam-new-arrival/sam_newArrival";
 import SamEventBanner from "../sam_eventBanner/sam_eventBanner";
+import SamDisplay1 from "../sam_display1/sam_display1";
 
 const SamMainContents = (props) => (
   <div className={styles.mainContents}>
     <SamNewArrival />
     <SamEventBanner />
+    <SamDisplay1 />
   </div>
 );
 
