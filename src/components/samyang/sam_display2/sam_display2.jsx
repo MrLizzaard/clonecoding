ef8bd9b41b16120cdc5acbbab45a13682661cda4
MrLizzaard <dhk21512@gmail.com>
2021-09-08 17:41:31 +0900

    display2

---
+++ src/components/samyang/sam_display2/sam_display2.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./sam_display2.module.css";
+
+const SamDisplay2 = (props) => <div>display2</div>;
+
+export default SamDisplay2;
--- src/components/samyang/sam_mainContents/sam_mainContents.jsx
+++ src/components/samyang/sam_mainContents/sam_mainContents.jsx
@@ -3,12 +3,14 @@ import styles from "./sam_mainContents.module.css";
 import SamNewArrival from "../sam-new-arrival/sam_newArrival";
 import SamEventBanner from "../sam_eventBanner/sam_eventBanner";
 import SamDisplay1 from "../sam_display1/sam_display1";
+import SamDisplay2 from "../sam_display2/sam_display2";
 
 const SamMainContents = (props) => (
   <div className={styles.mainContents}>
     <SamNewArrival />
     <SamEventBanner />
     <SamDisplay1 />
+    <SamDisplay2 />
   </div>
 );
 
