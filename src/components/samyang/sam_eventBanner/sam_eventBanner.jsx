85eab6f96465a27568c3b4a9c55445173622dab3
MrLizzaard <dhk21512@gmail.com>
2021-09-08 09:49:16 +0900

    eventBanner

--- src/components/samyang/sam_eventBanner/sam_eventBanner.jsx
+++ src/components/samyang/sam_eventBanner/sam_eventBanner.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./sam_eventBanner.module.css";
 
-const SamEventBanner = (props) => <h1>SamEventBanner</h1>;
+const SamEventBanner = (props) => (
+  <div className={styles.eventBanner}>
+    <Link>
+      <div className={styles.event1}></div>
+    </Link>
+  </div>
+);
 
 export default SamEventBanner;
