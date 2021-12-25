298721aaa3fb22783ab175aebd1aa83432a648c0
MrLizzaard <dhk21512@gmail.com>
2021-12-25 19:50:19 +0900

    sideMenu finish

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 const subMenu = document.querySelector(".menu-collapse-container");
 const subMenuBox = document.querySelector(".navbarSubShadow");
 const subMenuContainer = document.querySelector(".subMenuContainer");
+const sideMenuItem = document.querySelectorAll(".sidemenu__item");
 const navbar = document.querySelector("nav");
 
+// subMenu appear & disappear
 document.querySelectorAll(".item").forEach((item) => {
   item.addEventListener("mouseenter", () => {
     subMenu.classList.toggle("subMenu__mouseenter");
@@ -26,6 +28,13 @@ subMenuContainer.addEventListener("mouseleave", () => {
   subMenuBox.classList.toggle("navbarSubShadow__mouseenter");
 });
 
+// sidemenu appear & disappear
 document.querySelector(".navbar__hamburgur").addEventListener("click", () => {
   navbar.classList.toggle("hamburgur__click");
+});
+
+sideMenuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("sidemenu__on");
+  });
 });
