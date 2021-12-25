81246affad91f18e89f6741104ed0a77617e2c2b
MrLizzaard <dhk21512@gmail.com>
2021-12-25 18:49:54 +0900

    create side menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 
 const subMenu = document.querySelector(".menu-collapse-container");
 const subMenuBox = document.querySelector(".navbarSubShadow");
+const subMenuContainer = document.querySelector(".subMenuContainer");
+const navbar = document.querySelector("nav");
 
 document.querySelectorAll(".item").forEach((item) => {
   item.addEventListener("mouseenter", () => {
@@ -14,12 +16,16 @@ document.querySelectorAll(".item").forEach((item) => {
   });
 });
 
-subMenuBox.addEventListener("mouseenter", () => {
+subMenuContainer.addEventListener("mouseenter", () => {
   subMenu.classList.toggle("subMenu__mouseenter");
   subMenuBox.classList.toggle("navbarSubShadow__mouseenter");
 });
 
-subMenuBox.addEventListener("mouseleave", () => {
+subMenuContainer.addEventListener("mouseleave", () => {
   subMenu.classList.toggle("subMenu__mouseenter");
   subMenuBox.classList.toggle("navbarSubShadow__mouseenter");
+});
+
+document.querySelector(".navbar__hamburgur").addEventListener("click", () => {
+  navbar.classList.toggle("hamburgur__click");
 });
